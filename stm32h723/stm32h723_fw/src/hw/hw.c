2e421f45158f575779bdfea45d46afd9d84779df
Hancheol Cho <chcbaram@gmail.com>
2022-11-15 21:44:02 +0900

    펌웨어 사이즈 측정 추가

--- stm32h723/stm32h723_fw/src/hw/hw.c
+++ stm32h723/stm32h723_fw/src/hw/hw.c
@@ -2,6 +2,9 @@
 
 
 
+extern uint32_t _fw_flash_begin;
+extern uint32_t _fw_flash_end;
+
 
 
 bool hwInit(void)
@@ -17,6 +20,11 @@ bool hwInit(void)
   logPrintf("\r\n[ Firmware Begin... ]\r\n");
   logPrintf("Booting..Name \t\t: %s\r\n", _DEF_BOARD_NAME);
   logPrintf("Booting..Ver  \t\t: %s\r\n", _DEF_FIRMWATRE_VERSION);  
+  logPrintf("\n");
+
+  logPrintf("FW Addr Begin \t\t: 0x%X\r\n", &_fw_flash_begin);  
+  logPrintf("FW Addr End   \t\t: 0x%X\r\n", &_fw_flash_end);  
+  logPrintf("FW Addr Size  \t\t: %d bytes\r\n", (uint32_t)&_fw_flash_end - (uint32_t)&_fw_flash_begin);  
   logPrintf("\n");
 
   return true;
