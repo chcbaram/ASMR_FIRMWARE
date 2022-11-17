5cca76db276b042068469debf89f12075f1bef1c
Hancheol Cho <chcbaram@gmail.com>
2022-11-17 23:06:18 +0900

    .tag 정보 추가

--- stm32h723/stm32h723_fw/src/hw/hw.c
+++ stm32h723/stm32h723_fw/src/hw/hw.c
@@ -1,6 +1,23 @@
 #include "hw.h"
 
 
+typedef struct  
+{
+  uint32_t magic_number;
+  char version_str[32];
+} version_info_t;
+
+
+
+__attribute__((section(".tag"))) version_info_t version_info = 
+{
+  .magic_number = 0x5555AAAA,
+  .version_str = "V221117R1"
+};
+
+
+const version_info_t *p_version_info = (version_info_t *)0x08000400;
+
 
 extern uint32_t _fw_flash_begin;
 extern uint32_t _fw_flash_end;
@@ -26,6 +43,11 @@ bool hwInit(void)
   logPrintf("FW Addr End   \t\t: 0x%X\r\n", &_fw_flash_end);  
   logPrintf("FW Addr Size  \t\t: %d bytes\r\n", (uint32_t)&_fw_flash_end - (uint32_t)&_fw_flash_begin);  
   logPrintf("\n");
+
+  if (p_version_info->magic_number == 0x5555AAAA)
+  {
+    logPrintf("FW Version    \t\t: %s\n", p_version_info->version_str);
+  }
 
   return true;
 }
