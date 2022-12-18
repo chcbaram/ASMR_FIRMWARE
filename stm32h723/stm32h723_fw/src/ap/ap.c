3763888fa25f5985d3eb754bf827f7d80162566b
Hancheol Cho <chcbaram@gmail.com>
2022-12-19 00:03:28 +0900

    초기화되지 않는 변수 추가

--- stm32h723/stm32h723_fw/src/ap/ap.c
+++ stm32h723/stm32h723_fw/src/ap/ap.c
@@ -3,10 +3,33 @@
 
 
 
+typedef struct
+{
+  uint32_t magic_number;
+  uint32_t data;
+  uint32_t data_inv;
+} noinit_data_t;
+
+__attribute__((section(".noinit"))) noinit_data_t reset_count;
+
 
 void apInit(void)
 {
   cliOpen(_DEF_UART1, 115200);
+
+  if (reset_count.magic_number != 0x5555AAAA || 
+      reset_count.data != ~reset_count.data_inv)
+  {
+    // 초기화
+    reset_count.magic_number = 0x5555AAAA;
+    reset_count.data = 0;
+    reset_count.data_inv = ~reset_count.data;
+  }
+
+  reset_count.data++;
+  reset_count.data_inv = ~reset_count.data;
+
+  cliPrintf("Reset Count : %d\n", reset_count.data);
 }
 
 void apMain(void)
